5f2feb31821d21709412a8adf65a3058fe9bafe7
Samuel Mensah <samuelnmensah@live.com>
2016-03-16 22:09:57 -0700

    added website

---
+++ src/PacificFencing.Site/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PacificFencing.Site.MvcApplication" Language="C#" %>
---
+++ src/PacificFencing.Site/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PacificFencing.Site.Startup))]
+namespace PacificFencing.Site
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
